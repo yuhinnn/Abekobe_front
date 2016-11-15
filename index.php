0b8fca1f8cca3e31a8f80115a9288eb5b9f6c8bb
yuhi_deve <yuhinnn@gmail.com>
2016-11-15 17:03:41 +0900

    add .gitignore

--- index.php
+++ index.php
@@ -1,6 +1,77 @@
-
-hello Abekobe 
-
-
-       <img  src="img/top.jpg">
-
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">オモロタス</div>
+    <div class="header-right">
+      <ul>
+        <li class="selected">会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+  <div class="main">
+    <div class="sidebar">
+      <ul>
+        <li>オモロタスとは？</li>
+        <li>事業内容</li>
+        <li>プレスリリース</li>
+        <li>メンバー紹介</li>
+      </ul>
+      <img src="img/top.jpeg" width="250px">
+    </div>
+    
+    <div class="contents">
+      <div class="contents-top">
+        <h1>オモロタスとは？</h1>
+        <div class="contents-top-item">
+          <h2><span class="what-highlight">What</span> We Do</h2>
+          <p>『世の中にオモシロイを一つでも』をモットーに活動中。<p>
+        </div>
+        <div class="contents-top-item">
+          <h2><span class="why-highlight">Why</span> We Do</h2>
+          <p>アイドルで世界は変えられる。私達はそう信じています。また、アイドル界を我々が帰る。それが使命だと考えています。</p>
+        </div>
+        <div class="contents-top-item">
+          <h2><span class="how-highlight">How</span> We Do</h2>
+          <p>僕らが作ろうとしているアイドル。それは「参加型アイドル」。みんなで一緒に作り上げていくアイドルを目指します。</p>
+        </div>
+      </div>
+      
+      <div class="contents-bottom">
+        <h1>どんなことしているの？</h1>
+        <table>
+          <tr>
+            <th>活動</th>
+            <th>内容</th>
+          </tr>
+          <tr>
+            <td class="html-title">プロデュース</td>
+            <td>アイドルのライブ、握手会などの運営活動をしています</td>
+          </tr>
+          <tr>
+            <td class="css-title">YouTube</td>
+            <td>アイドルたちの日常、ライブなどを動画に残して配信しています</td>
+          </tr>
+          <tr>
+            <td class="php-title">IT</td>
+            <td>全て自分たちの力でホームページや物販ページの作成を行っています</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="footer">
+    <!-- <div>タグを追加し、「footer-left」というクラスをつけてください -->
+    
+    <!-- <div>タグを追加し、「like-box」というクラスをつけてください -->
+    
+  </div>
+</body>
+</html>/
