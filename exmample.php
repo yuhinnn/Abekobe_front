40dfdc67cbe60482a7800a58904b8c2fcc7f4d72
yuhi_deve <yuhinnn@gmail.com>
2016-11-17 05:13:46 +0900

    add jpg

--- exmample.php
+++ exmample.php
@@ -75,3 +75,79 @@
   </div>
 </body>
 </html>/
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>omorotasu</title>
+  <style type="text/css">
+BODY {
+ height:2800px;
+ background-repeat: no-repeat; 
+ background-position:50% 50%;
+ width:100%;
+}
+
+h1{
+  text-align:center;
+}
+h2{
+  text-align:center;
+  padding-top:45px;
+}
+p{
+  text-align:center;
+  font-size:78%;
+  margin-bottom:0;
+  
+}
+h3{
+  text-align:center;
+  font-size:140%;
+  padding-top:25px;
+}
+
+h4{
+  text-align:center;
+  font-size:160%;
+  padding-top:120px;
+  
+}
+
+h5{
+  text-align:center;
+  font-size:160%;
+  padding-top:360px;
+  margin-bottom:0;
+}
+
+.member{
+  padding-bottom:100px;
+}
+  </style>
+</head>
+<body background="img/page.jpg" width="100%">
+<h1><font face="Impact">オモロタスProduction</font></h1>
+<h2><font color="yellow">オモロタス</font>とは？</h2> </body> 
+<p>元々、メンバーはたくさんの人を『喜ばせて幸せにしたい』<br>と思い事務所をたてました。<br>私たちは、人が喜んでいる姿を見るのが大好きです。<br>
+では喜ばせるためには何が必要なのかと考えたんです。<br>『お金』『時間』<br>確かに喜ばれると思います。<br>でもこの二つがあれば『絶対に』<br>喜びが
+生まれるというわけではないですよね?<br>お金がたくさんあっても一緒に遊んでくれる人がいなかったら？<br>時間がたくさんあっても時間の使い方を知らなかったら？<br>
+人は喜びません幸せではないのでしょう<br>では、本当に喜びや幸せとはなんなのでしょうか？<br>それは、まさに<br>「笑っている瞬間』だと定義しています。
+<br>人が楽しい、面白いと心を動かし感動した瞬間<br>この笑いを増やせば<br>幸せになる人が必然的に増えると考えます。<br>では笑いってどうやって生まれるのでしょう。<br>
+オモロタス　プロダクション<br>の人間はその笑い、笑顔を生み出すことに<br>真剣にかつ楽しく取り組んでおります。<br>僕らと共に活動する人たちも<br>
+笑って幸せになってもらわないと<br>私たちの目的は果たされません。<br>特に身近な人を一番に考え、<br>身近な人から幸せになってもらうという、<br>『身近ファースト』
+<br>で運営を行います。<br>『笑いに本気で取り組む』<br>そんな人達と共に取り組み、<br>笑＝幸せを提供していくことに尽力する。</p>
+
+<h3>アイドルをプロデュースするにあたって</h3>
+<p>私たちは、アイドルをプロデュースし、<br>どんな価値を生み出したいか。<br>アイドルとして活動してくれる女の子の<br>未来の幅を広げること、そしてもう一つの家のような
+<br>素晴らしい環境を提供したいと考えて居ります。<br><br>なぜアイドルをプロデュースするのか。<br>根本は『笑顔』『幸せ』の追求です。<br>メンバーに笑顔になってもらいたい<br>
+生活に笑顔を増やして幸せになってもらいたいのです。<br>アイドルが笑顔になることで、<br>お客様が笑顔になる。<br>笑顔が増えることで、私たちも笑顔になる。<br>一つずつ笑顔の輪を広げていくことで、<br>
+お金などでは見出すことのできない<br>価値を見出すことができるのです。<br><br>日本で、いや世界で一番満足度が高い<br>プロダクションを目指して精進していきます。</p>
+
+<h4><font face="impact" color="red">募集</font></h4>
+
+<h5>メンバー紹介</h5>
+<p class="member">現在の運営のメンバーは<br>代表の渡邊が個人で集めたメンバーです。<br>集めた基準は、笑顔の数と人を笑顔にできているかです。<br>そんな素晴らしいメンバーで構成されています。</p>
+
+</html>
